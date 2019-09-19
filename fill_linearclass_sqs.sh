7c9f6e60e066d2055c3d54bc82c20c54d2ecbb1b
rhodricusack <rhodricusack@gmail.com>
2019-09-19 21:26:20 +0100

    Updated linear class path

--- fill_linearclass_sqs.sh
+++ fill_linearclass_sqs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LINEARCLASSBUCKET="neurana-imaging"
-LINEARCLASSPATH="rhodricusack/deepcluster_analysis/linearclass/"
+LINEARCLASSPATH="rhodricusack/deepcluster_analysis/linearclass_v2/"
 
 for ((tp=70;tp>=0;tp-=10)); do
   for ((conv=5;conv>0; conv--)); do
-      gotfile=$(aws s3 ls "s3://${LINEARCLASSBUCKET}/${LINEARCLASSPATH}linearclass_v2_time_${tp}_conv_${conv}")
+      gotfile=$(aws s3 ls "s3://${LINEARCLASSBUCKET}/${LINEARCLASSPATH}linearclass_time_${tp}_conv_${conv}")
       if [ -z "$gotfile" ]; then
             echo "Nothing found for time $tp conv $conv";
 
