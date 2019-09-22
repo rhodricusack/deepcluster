59fd2b87d6e9d742de2f46dc2dd0e48b2065b432
rhodricusack <rhodricusack@gmail.com>
2019-09-22 16:52:25 +0100

    short aoa toplayer runs

--- fill_linearclass_sqs_toplayer_epoch.sh
+++ fill_linearclass_sqs_toplayer_epoch.sh
@@ -3,10 +3,11 @@ LINEARCLASSBUCKET="neurana-imaging"
 LINEARCLASSPATH="rhodricusack/deepcluster_analysis/linearclass_v3/"
 
 for ((conv=5;conv>0; conv--)); do
-  for ((tp=2;tp<10;tp+=2)); do
-      gotfile=$(aws s3 ls "s3://${LINEARCLASSBUCKET}/${LINEARCLASSPATH}linearclass_time_${tp}_conv_${conv}/model_best.pth.tar")
+  for ((tp=0;tp<10;tp+=20)); do
+      aoapth="s3://${LINEARCLASSBUCKET}/${LINEARCLASSPATH}linearclass_time_${tp}_conv_${conv}/aoaresults_toplayer_epoch_1.json"
+      gotfile=$(aws s3 ls "$aoapth")
       if [ -z "$gotfile" ]; then
-            echo "Time $tp conv $conv not previously run";
+            echo "Time $tp conv $conv not previously run $aoapth";
 
             aws sqs send-message --queue-url "https://sqs.eu-west-1.amazonaws.com/807820536621/deepcluster-linearclass.fifo" \
                 --message-body "{\"epoch\":$tp,\"conv\":$conv}" \
