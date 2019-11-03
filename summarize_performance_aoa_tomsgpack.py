6f8b322637adeed99f77be2d145da0668ac2655c
rhodricusack <rhodricusack@gmail.com>
2019-11-03 10:39:54 +0000

    updating fsx paths

--- summarize_performance_aoa_tomsgpack.py
+++ summarize_performance_aoa_tomsgpack.py
@@ -14,7 +14,7 @@ with open('notfound.csv','w') as notfoundf:
     notfoundf.write('stage\tconv\n')
     for stage in list(range(0,10,2))+list(range(20,70,20)):
         for conv in range(1,6,1):
-            lcpth='/home/ubuntu/linearclass_v3/linearclass_time_%d_conv_%d'%(stage,conv)
+            lcpth='/fsx/rhodricusack/iclr2020/deepcluster_analysis/linearclass_v3/linearclass_time_%d_conv_%d'%(stage,conv)
             print('Loading %s'%lcpth)
             aoapth=path.join(lcpth,'aoaresults_toplayer_epoch_1.json')  
             if os.path.exists(aoapth):
@@ -26,6 +26,6 @@ with open('notfound.csv','w') as notfoundf:
                         df_aoa=df_aoa.append(pd.DataFrame.from_dict(d),ignore_index=True)
             else:
                 notfoundf.write('%d\t%d\n'%(stage,conv))
-                
+
 df_aoa.to_json('linearclass_v3_aoa.json')
 print(df_aoa)
