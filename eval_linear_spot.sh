9250b4c64a04ad9e84bbcb58cfa7d9b6c2b079e6
rhodricusack <rhodricusack@gmail.com>
2019-11-04 15:00:39 +0000

    EBS for data and S3 for checkpoints

--- eval_linear_spot.sh
+++ eval_linear_spot.sh
@@ -8,7 +8,7 @@
 
 DATA="/imagenet"
 
-PYTHON="~/anaconda3/bin/python"
+PYTHON="../anaconda3/bin/python"
 
 CHECKPOINTBUCKET="neurana-imaging"
 CHECKPOINTPATH="rhodricusack/deepcluster_analysis/checkpoints_2019-09-11/checkpoints"
@@ -16,4 +16,6 @@ LINEARCLASSBUCKET="neurana-imaging"
 LINEARCLASSPATH="rhodricusack/deepcluster_analysis/linearclass_v3/"
 
 mkdir -p ${EXP}_${TIMEPOINT}
+
+cd deepcluster
 ${PYTHON}  eval_linear_spot.py --data ${DATA} --epochs 2 --lr 0.01 --wd -7 --verbose --workers 4 --conv ${CONV} --timepoint ${TIMEPOINT}  --checkpointbucket ${CHECKPOINTBUCKET} --checkpointpath ${CHECKPOINTPATH} --linearclassbucket ${LINEARCLASSBUCKET} --linearclasspath ${LINEARCLASSPATH} --aoaval --toplayer_epoch ${TOPLAYER_EPOCH} &
